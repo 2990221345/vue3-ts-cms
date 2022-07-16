54dc4eabf545649db512273700d082527cbf62b9
Peach <2990221345@qq.com>
2022-07-16 10:36:07 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to My Peach Blog" />
+  <HelloWorld msg="Welcome to Peach Blog" />
 </template>
 
 <script lang="ts">
