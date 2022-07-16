77a131cc67f2a26e64b9263e18ddc2f1d7385520
Peach <2990221345@qq.com>
2022-07-16 12:55:32 +0800

    feat(config.js): vue.config.js 配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
+const path = require('path')
 module.exports = {
   publicPath: './',
-  outputDir: 'docs'
+  outputDir: 'docs',
+  // 配置方式二：和webpack属性完全一致，最后会进行合并
+  // configureWebpack: {
+  //   resolve: {
+  //     alias: {
+  //       components: '@/components'
+  //     }
+  //   }
+  // }
+  // configureWebpack: (config) => {
+  //   config.resolve.alias = {
+  //     '@': path.resolve(__dirname, 'src')
+  //   }
+  // }
+  // 配置方式三
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', path.resolve(__dirname, 'src'))
+      .set('components', path.resolve(__dirname, '@/components'))
+  }
 }
