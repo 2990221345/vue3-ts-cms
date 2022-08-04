aa993156b52b7c97e19d6e17d19358e21d83690b
Peach <2990221345@qq.com>
2022-08-05 00:02:34 +0800

    feat(layout): display

--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>main</div>
-  <div>
-    <router-view></router-view>
+  <div class="layout">
+    <div>main</div>
+    <div>
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style lang="less" scoped>
+.layout {
+  display: flex;
+  justify-content: space-evenly;
+}
+</style>
