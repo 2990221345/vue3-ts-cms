e5060ebe6d11f3601896c4209bf43fa62a699cbd
Peach <2990221345@qq.com>
2022-08-14 00:09:40 +0800

    feat(glocal): n
    n

--- src/global/register-properties.ts
+++ src/global/register-properties.ts
@@ -2,10 +2,11 @@ import { App } from 'vue'
 import { formatUtcString } from '@/utils/date-format'
 export default function registerProperties(app: App) {
   // 全局过滤方法
+  /**
+   *   $filters.formatTime(value)
+   *  @return '2022-08-14 22:00'
+   */
   app.config.globalProperties.$filters = {
-    foo() {
-      console.log('foo')
-    },
     formatTime(value: string) {
       return formatUtcString(value)
     }
