998cd7ac3edde2947a2a618c13a47410c4de14c6
Peach <2990221345@qq.com>
2022-08-13 19:47:55 +0800

    feat(table): table作用域插槽

---
+++ src/base-ui/table/src/table.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <el-table :data="props.listData" border style="width: 100%">
+      <template v-for="propItem in props.propList" :key="propItem.prop">
+        <el-table-column v-bind="propItem" align="center">
+          <template #default="scope">
+            <slot :name="propItem.slotName" :row="scope.row">
+              {{ scope.row[propItem.prop] }}
+            </slot>
+          </template>
+        </el-table-column>
+      </template>
+    </el-table>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+const props = defineProps({
+  listData: {
+    type: Array,
+    required: true
+  },
+  propList: {
+    type: Array,
+    required: true
+  }
+})
+</script>
+
+<style lang="less" scoped></style>
--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -2,8 +2,16 @@
   <div class="user">
     <PageSearch :SearchFromConfig="SearchFromConfig"></PageSearch>
     <div class="content">
-      {{ userList }}
-      {{ userCount }}
+      <HyTable :listData="userList" :propList="propList">
+        <template #status="scope">
+          <el-tag>{{ scope.row.enable ? '启用' : '禁用' }}</el-tag>
+        </template>
+        <template #createAt="scope">
+          <strong>{{ scope.row.createAt }}</strong>
+        </template>
+      </HyTable>
+      <!--
+      <el-pagination layout="prev, pager, next" :total="userCount" /> -->
     </div>
   </div>
 </template>
@@ -11,8 +19,10 @@
 <script lang="ts" setup>
 import { computed } from 'vue'
 import PageSearch from '@/components/page-search'
+import HyTable from '@/base-ui/table'
 import { SearchFromConfig } from './config/serch.config'
 import { useStore } from 'vuex'
+
 const store = useStore()
 store.dispatch('system/getPageListAction', {
   pageUrl: '/users/list',
@@ -24,6 +34,24 @@ store.dispatch('system/getPageListAction', {
 
 const userList = computed(() => store.state.system.userList)
 const userCount = computed(() => store.state.system.userCount)
+const propList = [
+  { prop: 'name', label: '用户名', minWidth: '100' },
+  { prop: 'realname', label: '真实姓名', minWidth: '100' },
+  { prop: 'cellphone', label: '电话号码', minWidth: '120' },
+  { prop: 'enable', label: '状态', minWidth: '100', slotName: 'status' },
+  {
+    prop: 'createAt',
+    label: '创建时间',
+    minWidth: '250',
+    slotName: 'createAt'
+  },
+  { prop: 'updateAt', label: '更新时间', minWidth: '250', slotName: 'updateAt' }
+]
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.content {
+  border-top: 20px solid #f5f5f5;
+  padding: 20px;
+}
+</style>
