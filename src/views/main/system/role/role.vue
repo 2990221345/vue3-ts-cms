426587098dd61f9c66fe3a7aa021102ed0fc22cc
Peach <2990221345@qq.com>
2022-08-17 23:06:30 +0800

    feat(update): role

--- src/views/main/system/role/role.vue
+++ src/views/main/system/role/role.vue
@@ -1,9 +1,35 @@
 <template>
-  <div class="role">
-    <h2>role</h2>
+  <div class="user">
+    <!-- <PageSearch :SearchFromConfig="SearchFromConfig"></PageSearch> -->
+    <PageContent :contentTableConfig="contentTableConfig"></PageContent>
+    <div class="content">
+      <!-- :title="title"
+        :propList="propList"
+        :showIndexColumn="showIndexColumn"
+        :showSelectColumn="showSelectColumn" -->
+      <!--
+      <el-pagination layout="prev, pager, next" :total="userCount" /> -->
+    </div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useStore } from 'vuex'
 
-<style scoped></style>
+// 页面配置
+// import PageSearch from '@/components/page-search'
+import PageContent from '@/components/page-content'
+// import { SearchFromConfig } from './config/serch.config'
+import { contentTableConfig } from './config/content.config'
+
+const store = useStore()
+store.dispatch('system/getPageListAction', {
+  pageName: 'user',
+  queryInfo: {
+    offset: 0,
+    size: 10
+  }
+})
+</script>
+
+<style lang="less" scoped></style>
