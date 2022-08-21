2c0fd4e234e0d006c716e2f90a42c7c4993fa8f8
Peach <2990221345@qq.com>
2022-08-21 23:12:49 +0800

    feat(update): update demo.js

--- demo.js
+++ demo.js
@@ -58,7 +58,7 @@ for (let key in result23) {
 // 用js编写一个方法，输出字符串中连续最多的字符及次数
 
 console.log('result23', keyStr, max)
-function getStrMaxCount(str) {
+function getStrMaxCount (str) {
   var resultStr = ''
   var resultCount = 0
   var i = 0
@@ -107,3 +107,35 @@ function getStrMaxCount(str) {
 // const newRemoval1 = removal.reduce((item, next) => {
 //   console.log(item, next)
 // }, [])
+
+// 实现防抖函数
+const debounce = (func, delay) => {
+  let timer
+  // 这里返回的函数是每次用户实际调用的防抖函数
+  // 如果已经设定过定时器了就清空上一次的定时器
+  // 开始一个新的定时器，延迟执行用户传入的方法
+  return function (...args) {
+    if (timer) clearTimeout
+    timer = setTimeout(() => {
+      func.apply(this, args)
+    }, delay)
+  }
+}
+// 实现节流函数
+const throttle = (func, wait = 50) => {
+  let lastTime = 0
+  return function (...args) {
+    // 当前时间
+    let now = +new Date()
+    // 将当前时间和上一次执行函数时间对比
+    // 如果差值大于设置的等待时间就执行函数
+    if (now - lastTime > wait) {
+      lastTime = now
+      func.apply(this, args)
+    }
+  }
+}
+console.log('节流')
+throttle(() => {
+  console.log(1)
+}, 200)
