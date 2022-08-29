a953997bdc9f3ebb57237b314b1a0e209f35fd69
Peach <2990221345@qq.com>
2022-08-29 23:37:42 +0800

    feat(menu): add menu

--- src/views/main/system/menu/menu.vue
+++ src/views/main/system/menu/menu.vue
@@ -1,19 +1,56 @@
 <template>
   <div class="menu">
-    <h2>menu</h2>
-    <!-- <PageSearch></PageSearch> -->
+    <PageSearch
+      :SearchFromConfig="SearchFromConfig"
+      @resetBtnClick="handleResetClick"
+      @queryBtnClick="handleQueryClick"
+    ></PageSearch>
+    <PageContent
+      ref="pageContentRef"
+      :contentTableConfig="contentTableConfig"
+      pageName="menu"
+    >
+      <!-- 状态插槽 -->
+      <template #status="scope">
+        <el-button
+          plain
+          size="small"
+          :type="scope.row.enable ? 'success' : 'danger'"
+          >{{ scope.row.enable ? '启用' : '禁用' }}</el-button
+        >
+      </template>
+    </PageContent>
+    <div class="content">
+      <!-- :title="title"
+        :propList="propList"
+        :showIndexColumn="showIndexColumn"
+        :showSelectColumn="showSelectColumn" -->
+      <!--
+      <el-pagination layout="prev, pager, next" :total="menuCount" /> -->
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-// import PageSearch from '@/components/page-search'
-export default defineComponent({
-  name: 'menu',
-  setup() {
-    return {}
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { useStore } from 'vuex'
+import { usePageSearch } from '@/hooks/usePageSearch'
+// 页面配置
+import PageSearch from '@/components/page-search'
+import PageContent from '@/components/page-content'
+import { SearchFromConfig } from './config/content.config'
+import { contentTableConfig } from './config/content.config'
+
+const store = useStore()
+store.dispatch('system/getPageListAction', {
+  pageName: 'menu',
+  queryInfo: {
+    offset: 0,
+    size: 10
   }
 })
+// 执行hook
+const [pageContentRef, handleResetClick, handleQueryClick] = usePageSearch()
 </script>
 
-<style scoped></style>
+<style lang="less" scoped></style>
